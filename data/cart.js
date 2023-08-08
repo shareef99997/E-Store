d8b8d8bfd07cd9f447d04c11b727f17f1c5dcd8a
Your Nameemail <shareef.99997@gmail.com>
2023-08-08 22:22:10 +0300

    genarate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart =[]
+export const cart =[{
+  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+  quantity:2,
+},{
+  productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  quantity:2,
+}
+]
 
 export function addToCart(productId) {
   let matchingItem;
