8428364644bf09096d455437056b9a1dbbc0ec08
Your Nameemail <shareef.99997@gmail.com>
2023-08-09 13:30:03 +0300

    save cart items in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,19 @@
-export let cart =[{
-  productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity:2,
-},{
-  productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  quantity:2,
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if (!cart) {
+  cart = [{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:2,
+  },{
+    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    quantity:2,
+  }
+  ]
 }
-]
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -24,7 +32,10 @@ export function addToCart(productId) {
         quantity: 1
       });
     }
+    saveToStorage();
 }
+
+
 
 export function removeFromeCart(productId) {
   const newCart = [];
@@ -36,4 +47,8 @@ export function removeFromeCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 };
+
+
